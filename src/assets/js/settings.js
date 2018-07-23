07491ce38c148be30959ebeab5654181829c03d9
Aliasgar Murtaza <aliasgarlabs@gmail.com>
2018-07-23 16:50:07 +0530

    Add download implementation and change wallpaper library

--- src/assets/js/settings.js
+++ src/assets/js/settings.js
@@ -1,12 +1,32 @@
-const wallpaper = require('wallpaper-js');
 const axios = require('axios');
+const wallpaper= require('wallpaper');
+
+var fs = require('fs'),
+    request = require('request');
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 // require syntax
 
 function changeWallpaper(sourceUrl) {
   let options = { source : sourceUrl}
-  wallpaper.set(options);
+
+  download(sourceUrl, 'wallpapers/wall.jpg', function(){
+    console.log('download done');
+    wallpapernew.set('wallpapers/wall.jpg').then(() => {
+    	console.log('wallpaper set');
+    });
+  });
 }
 
 
